4f0c0b9c17dcd76bd8ff184628450470f4cc5917
= <81330314+lightman210567@users.noreply.github.com>
2023-10-12 18:02:09 +0100

    created the draw function for "a"
    created a function to draw the futhorc rune for the letter "a"
    
    modified: src/futhorc/futhorc.py
    
    Signed-off-by: = <81330314+lightman210567@users.noreply.github.com>

---
+++ src/futhorc/futhorc.py
@@ -0,0 +1,20 @@
+import turtle
+
+def drawA():
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.right(135)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.right(180)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.left(135)
+    turtle.forward(25)
+    turtle.left(45)
+    turtle.forward(50)
+
+drawA()
+turtle.done()
